7b80d1d96aae2fe12059d7571a22fe68c2fb996a
Stephen Floor <floor@berkeley.edu>
2014-10-08 14:54:41 -0700

    adding source - snf

---
+++ knowngenes_to_transcript_regions.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+# Stephen N. Floor 
+# 7 October 2014 
+# floor@berkeley.edu 
+
+# TODO: 
+
+# convert UCSC gene names to refseq?
+
+import sys, os
+from snfGene import * 
+
+print " ---------------------------------"
+print "| Extract Regions from knownGenes |"
+print "|  snf   7 October 2014           |"
+print " ---------------------------------\n\n"
+
+if len(sys.argv) != 3:
+    sys.exit("ERROR: Please provide the knownGenes file and output basename as inputs")
+
+
+# output filenames: 
+utr5FName = sys.argv[2] + "_5utr.bed"
+cdsFName = sys.argv[2] + "_cds.bed"
+utr3FName = sys.argv[2] + "_3utr.bed"
+exonFName = sys.argv[2] + "_exons.bed"
+intronFName = sys.argv[2] + "_introns.bed"
+codingExonFName = sys.argv[2] + "_codingexons.bed"
+codingIntronFName = sys.argv[2] + "_codingintrons.bed" # note that these are introns from coding genes, not necessarily introns that make it to mRNA 
+noncodingExonFName = sys.argv[2] + "_noncodingexons.bed" 
+noncodingIntronFName = sys.argv[2] + "_noncodingintrons.bed" 
+
+#keep track of where we are 
+genesRead = 0
+
+#terminate if output files exist
+
+if os.path.exists(utr5FName) or os.path.exists(cdsFName) or os.path.exists(utr3FName) or os.path.exists(exonFName) or os.path.exists(intronFName) \
+        or os.path.exists(codingExonFName) or os.path.exists(codingIntronFName) or os.path.exists(noncodingExonFName) or os.path.exists(noncodingIntronFName):
+    sys.exit("ERROR: output basename %s files already exist" % sys.argv[2]) 
+
+#process the file
+
+with open(sys.argv[1]) as knownGenesFile, open(utr5FName, "w") as utr5File, open(cdsFName, "w") as cdsFile, \
+        open(utr3FName, "w") as utr3File, open(exonFName, "w") as exonFile, open (intronFName, "w") as intronFile, \
+        open(codingExonFName, "w") as codingExonFile, open(codingIntronFName, "w") as codingIntronFile, \
+        open(noncodingExonFName, "w") as noncodingExonFile, open(noncodingIntronFName, "w") as noncodingIntronFile:
+
+    for line in knownGenesFile:
+        # all of the knowngenes parsing and metadata construction is done inside snfGene.py, especially the createGene method 
+        gene = createGene(line)
+        genesRead += 1
+
+        if(gene.coding): 
+            for entry in gene.bedFormat(region="5utr"):
+                utr5File.write(entry + "\n")
+            for entry in gene.bedFormat(region="cds"):
+                cdsFile.write(entry + "\n")
+            for entry in gene.bedFormat(region="3utr"):
+                utr3File.write(entry + "\n")
+
+            for entry in gene.bedFormat(region="exons"):
+                exonFile.write(entry + "\n")
+                codingExonFile.write(entry + "\n")
+            
+            for entry in gene.bedFormat(region="introns"):
+                intronFile.write(entry + "\n")
+                codingIntronFile.write(entry + "\n")
+            
+        else: # noncoding transcripts just have exons and introns 
+            for entry in gene.bedFormat(region="exons"):
+                exonFile.write(entry + "\n")
+                noncodingExonFile.write(entry + "\n")
+            
+            for entry in gene.bedFormat(region="introns"):
+                intronFile.write(entry + "\n")
+                noncodingIntronFile.write(entry + "\n")
+
+        if (not genesRead % 1000):
+            print "Processed %d genes..." %  genesRead
+
+print "Processed %d genes." %  genesRead
---
+++ snfGene.py
@@ -0,0 +1,237 @@
+# class to create genes out of lines in knowngene file 
+
+# TODO: 
+
+# pretty sure 5' UTR length math is wrong for - strand at least
+# double check region parsing 
+# make method for BED format output 
+
+class snfGene: 
+    def __init__(self):
+        #properties defined in UCSC knowngenes 
+        self.name = '' 
+        self.chrom = ''
+        self.strand = '' 
+        self.txStart = 0
+        self.txEnd = 0 
+        self.cdsStart = 0
+        self.cdsEnd = 0
+        self.exonCt = 0
+        self.exonStarts = []
+        self.exonEnds = []
+        self.exonLengths = []
+        
+        #meta properties to be computed during construction.  these are lists of BED first four field tuples with the exception of Len terms which are the length of the total region for the gene 
+        self.utr5 = []
+        self.utr5Len = 0
+        self.cds = []
+        self.cdsLen = 0
+        self.utr3 = []
+        self.utr3Len = 0
+        self.exonicRegions = []
+        self.exonicRegionsLen = 0
+        self.intronicRegions = []
+        self.intronicRegionsLen = 0
+    
+        self.coding = False
+
+
+    def __str__(self):  #currently roughly knownGenes format with a second line containing metadata 
+        return "%s\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%s\t%s\n%s\t%d\t%s\t%d\t%s\t%d\t%s\t%d\t%s\t%d" % (self.name, self.chrom, self.strand, self.txStart, self.txEnd, self.cdsStart, self.cdsEnd, self.exonCt, self.exonStarts, self.exonEnds, self.utr5, self.utr5Len, self.cds, self.cdsLen, self.utr3, self.utr3Len, self.exonicRegions, self.exonicRegionsLen, self.intronicRegions, self.intronicRegionsLen)
+    
+#BED format output is goal.  Fields are optional after featureEnd 
+# chrom    featureStart   featureEnd   nameOfLine   score(0-1000)   strand   thickStart  thickEnd  itemRGBtuple  blockCount  blockSizes   blockStarts 
+
+#this function returns a list of BED-formatted strings for the feature passed as region 
+    def bedFormat(self, region="exons"):
+        if (not self.coding and (region == "5utr" or region == "cds" or region == "3utr")):
+            print "snfGene bedFormat error: noncoding transcripts do not have 5utr/cds/3utr"
+            return []
+
+        returnVal = []
+
+        if (region == "5utr"):
+            for chunk in self.utr5:
+                returnVal.append("%s\t%d\t%d\t%s\t0\t%d\t%d\t0".expandtabs() % (chunk[0], chunk[1], chunk[2], chunk[3]+"_5utr",chunk[1],chunk[2]))
+
+        elif (region == "cds"):
+            for chunk in self.cds:
+                returnVal.append("%s\t%d\t%d\t%s\t0\t%d\t%d\t0".expandtabs() % (chunk[0], chunk[1], chunk[2], chunk[3]+"_cds",chunk[1],chunk[2]))
+
+        elif (region == "3utr"):
+            for chunk in self.utr3:
+                returnVal.append("%s\t%d\t%d\t%s\t0\t%d\t%d\t0".expandtabs() % (chunk[0], chunk[1], chunk[2], chunk[3]+"_3utr",chunk[1],chunk[2]))
+
+        elif (region == "exons"):
+            for chunk in self.exonicRegions:
+                returnVal.append("%s\t%d\t%d\t%s\t0\t%d\t%d\t0".expandtabs() % (chunk[0], chunk[1], chunk[2], chunk[3]+"_exon",chunk[1],chunk[2]))
+
+        elif (region == "introns"):
+            for chunk in self.intronicRegions:
+                returnVal.append("%s\t%d\t%d\t%s\t0\t%d\t%d\t0".expandtabs() % (chunk[0], chunk[1], chunk[2], chunk[3]+"_intron",chunk[1],chunk[2]))
+        else:
+            print "snfGene bedFormat error: currently only regions 5utr/cds/3utr/exons/introns are supported"
+            
+
+        return returnVal
+        
+            
+
+#example line format from knownGenes file (from UCSC) 
+    #    uc010nxq.1      chr1    +       11873   14409   12189   13639   3       11873,12594,13402,      12227,12721,14409,      B7ZGX9  uc010nxq.1
+    # line format
+    #    name            chrom   strand  txStart txEnd   cdsStart cdsEnd exonCt  exonStarts              exonEnds                proteinID  alignID 
+
+def createGene(knownGeneLineString):
+    foo = snfGene()
+    line = knownGeneLineString.split()
+
+    # -- read in knownGene fields -- 
+
+    foo.name = line[0]
+    foo.chrom = line[1]
+    foo.strand = line[2]
+    foo.txStart = int(line[3])
+    foo.txEnd = int(line[4])
+    foo.cdsStart = int(line[5])
+    foo.cdsEnd = int(line[6])
+    foo.exonCt = int(line[7])
+    
+    starts = line[8].split(",")
+    ends = line[9].split(",")
+
+    for i in range(foo.exonCt):
+        foo.exonStarts.append(int(starts[i]))
+        foo.exonEnds.append(int(ends[i]))
+
+    # not doing it this way because of the trailing , in the knowngenes format 
+    #for i in line[8].split(","): foo.exonStarts.append(i)
+    #for i in line[9].split(","): foo.exonEnds.append(i)
+
+    # -- begin computing metadata -- 
+
+    # -- note: chose clarity of code and conditionals here over most efficient computation (i.e. some clauses may be redundant)
+
+    if (foo.strand == "+"): 
+        #print ("DBUG - exonCt %d i %d exonEnds[i] %d cdsStart %d exonStarts[i] %d cdsEnd %d") % \
+        #    (foo.exonCt, i, foo.exonEnds[i], foo.cdsStart, foo.exonStarts[i], foo.cdsEnd)
+        for i in range (foo.exonCt): 
+            if (foo.cdsStart != foo.cdsEnd): # if this is a coding transcript
+                foo.coding = True
+                # -- first compute 5'utr, CDS, 3'utr regions --
+                #case 1 - exon spans 5' UTR/CDS/3' UTR
+                if (foo.exonStarts[i] < foo.cdsStart and foo.exonEnds[i] > foo.cdsEnd):
+                    foo.utr5.append((foo.chrom, foo.exonStarts[i], foo.cdsStart, foo.name))
+                    foo.utr5Len += foo.cdsStart - foo.exonStarts[i]
+                    foo.cds.append((foo.chrom, foo.cdsStart, foo.cdsEnd, foo.name))
+                    foo.cdsLen += foo.cdsEnd - foo.cdsStart
+                    foo.utr3.append((foo.chrom, foo.cdsEnd, foo.exonEnds[i], foo.name))
+                    foo.utr3Len += foo.exonEnds[i] - foo.cdsEnd
+                #case 2 - exon spans 5' UTR/CDS junction
+                elif (foo.exonStarts[i] < foo.cdsStart and foo.exonEnds[i] >= foo.cdsStart):
+                    foo.utr5.append((foo.chrom, foo.exonStarts[i], foo.cdsStart, foo.name))
+                    foo.utr5Len += foo.cdsStart - foo.exonStarts[i]
+                    foo.cds.append((foo.chrom, foo.cdsStart, foo.exonEnds[i], foo.name))
+                    foo.cdsLen += foo.exonEnds[i]- foo.cdsStart
+                #case 3 - exon spans CDS/3'UTR junction 
+                elif (foo.exonStarts[i] >= foo.cdsStart and foo.exonStarts[i] <= foo.cdsEnd and foo.exonEnds[i] > foo.cdsEnd):
+                    foo.cds.append((foo.chrom, foo.exonStarts[i], foo.cdsEnd, foo.name))
+                    foo.cdsLen += foo.cdsEnd - foo.exonStarts[i]
+                    foo.utr3.append((foo.chrom, foo.cdsEnd, foo.exonEnds[i], foo.name))
+                    foo.utr3Len += foo.exonEnds[i] - foo.cdsEnd
+                #case 4 - exon is 5' UTR only 
+                elif (foo.exonStarts[i] < foo.cdsStart and foo.exonEnds[i] < foo.cdsStart): 
+                    foo.utr5.append((foo.chrom, foo.exonStarts[i], foo.exonEnds[i], foo.name))
+                    foo.utr5Len += foo.exonEnds[i] - foo.exonStarts[i]
+                #case 5 - exon is CDS only
+                elif (foo.exonStarts[i] >= foo.cdsStart and foo.exonEnds[i] <= foo.cdsEnd):
+                    foo.cds.append((foo.chrom, foo.exonStarts[i], foo.exonEnds[i], foo.name))
+                    foo.cdsLen += foo.exonEnds[i] - foo.exonStarts[i]
+                #case 6 - exon is 3' UTR only 
+                elif (foo.exonStarts[i] > foo.cdsEnd and foo.exonEnds[i] > foo.cdsEnd):
+                    foo.utr3.append((foo.chrom, foo.exonStarts[i], foo.exonEnds[i], foo.name))
+                    foo.utr3Len += foo.exonEnds[i] - foo.exonStarts[i]
+                else: 
+                    print "Thar be dragons - snfGene createGene + stranded gene region parsing" 
+            #else: 
+            #    print "noncoding + strand transcript" 
+                
+            # -- generate combined exonic and intronic regions -- 
+            #exons are easy 
+            foo.exonicRegions.append((foo.chrom, foo.exonStarts[i], foo.exonEnds[i], foo.name))
+            foo.exonicRegionsLen += foo.exonEnds[i] - foo.exonStarts[i]
+                
+            #print "DBUG2: i %d foo.exonCt-1 %d foo.exonEnds %s foo.exonStarts %s" % (i, foo.exonCt-1, foo.exonEnds, foo.exonStarts)
+        
+            if (i < foo.exonCt - 1): # only compute introns for nonterminal exons
+                # an intron is the region between the end of the current exon and start of the next 
+                foo.intronicRegions.append((foo.chrom, foo.exonEnds[i], foo.exonStarts[i+1], foo.name))
+                foo.intronicRegionsLen += foo.exonStarts[i+1] - foo.exonEnds[i] 
+
+    elif (foo.strand == "-"):
+     #uc001ach.2	    chr1    -	    910578  917473  911551  916546  5	    910578,911878,914260,916516,917444,	    911649,912004,916037,916553,917473,	    Q5SV97  uc001ach.2
+     #	name		chrom	strand	txStart txEnd	cdsStart foo.cdsEnd exonCt	exonStarts		exonEnds		proteinID  alignID 
+     # for the minus strand everything is the same except the order of encountering regions is reversed
+     # i.e. 3' UTR -> CDS -> 5' UTR 
+
+        for i in range (foo.exonCt): 
+            #print ("DBUG - exonCt %d i %d foo.exonEnds[i] %d foo.cdsStart %d exonStarts[i] %d foo.cdsEnd %d") % \
+            #    (foo.exonCt, i, foo.exonEnds[i], foo.cdsStart, foo.exonStarts[i], foo.cdsEnd)
+            
+            if (foo.cdsStart != foo.cdsEnd):
+                foo.coding = True 
+                # -- first compute 5'utr, CDS, 3'utr regions --
+                # -- this is the same as for + sense except 5' UTR and 3' UTR are swapped throughout
+                #case 1 - exon spans 3' UTR/CDS/5' UTR
+                if (foo.exonStarts[i] < foo.cdsStart and foo.exonEnds[i] > foo.cdsEnd):
+                    foo.utr3.append((foo.chrom, foo.exonStarts[i], foo.cdsStart, foo.name))
+                    foo.utr3Len += foo.cdsStart - foo.exonStarts[i]
+                    foo.cds.append((foo.chrom, foo.cdsStart, foo.cdsEnd, foo.name))
+                    foo.cdsLen += foo.cdsEnd - foo.cdsStart
+                    foo.utr5.append((foo.chrom, foo.cdsEnd, foo.exonEnds[i], foo.name))
+                    foo.utr5Len += foo.exonEnds[i] - foo.cdsEnd
+                #case 2 - exon spans 3' UTR/CDS junction
+                elif (foo.exonStarts[i] < foo.cdsStart and foo.exonEnds[i] >= foo.cdsStart):
+                    foo.utr3.append((foo.chrom, foo.exonStarts[i], foo.cdsStart, foo.name))
+                    foo.utr3Len += foo.cdsStart - foo.exonStarts[i]
+                    foo.cds.append((foo.chrom, foo.cdsStart, foo.exonEnds[i], foo.name))
+                    foo.cdsLen += foo.exonEnds[i]- foo.cdsStart
+                #case 3 - exon spans CDS/5'UTR junction 
+                elif (foo.exonStarts[i] >= foo.cdsStart and foo.exonStarts[i] <= foo.cdsEnd and foo.exonEnds[i] > foo.cdsEnd):
+                    foo.cds.append((foo.chrom, foo.exonStarts[i], foo.cdsEnd, foo.name))
+                    foo.cdsLen += foo.cdsEnd - foo.exonStarts[i]
+                    foo.utr5.append((foo.chrom, foo.cdsEnd, foo.exonEnds[i], foo.name))
+                    foo.utr5Len += foo.exonEnds[i] - foo.cdsEnd
+                #case 4 - exon is 3' UTR only 
+                elif (foo.exonStarts[i] < foo.cdsStart and foo.exonEnds[i] < foo.cdsStart): 
+                    foo.utr3.append((foo.chrom, foo.exonStarts[i], foo.exonEnds[i], foo.name))
+                    foo.utr3Len += foo.exonEnds[i] - foo.exonStarts[i]
+                #case 5 - exon is CDS only
+                elif (foo.exonStarts[i] >= foo.cdsStart and foo.exonEnds[i] <= foo.cdsEnd):
+                    foo.cds.append((foo.chrom, foo.exonStarts[i], foo.exonEnds[i], foo.name))
+                    foo.cdsLen += foo.exonEnds[i] - foo.exonStarts[i]
+                #case 6 - exon is 5' UTR only 
+                elif (foo.exonStarts[i] > foo.cdsEnd and foo.exonEnds[i] > foo.cdsEnd):
+                    foo.utr5.append((foo.chrom, foo.exonStarts[i], foo.exonEnds[i], foo.name))
+                    foo.utr5Len += foo.exonEnds[i] - foo.exonStarts[i]
+                else: 
+                    print "Thar be dragons - snfGene createGene - stranded gene region parsing" 
+                    
+            #else: 
+            #    print "- strand noncoding transcript"
+                
+            # -- generate combined exonic and intronic regions -- 
+            #exons are easy 
+            foo.exonicRegions.append((foo.chrom, foo.exonStarts[i], foo.exonEnds[i], foo.name))
+            foo.exonicRegionsLen += foo.exonEnds[i] - foo.exonStarts[i]
+            
+            if (i < foo.exonCt - 1): # only compute introns for nonterminal exons
+                # an intron is the region between the end of the current exon and start of the next 
+                foo.intronicRegions.append((foo.chrom, foo.exonEnds[i], foo.exonStarts[i+1], foo.name))
+                foo.intronicRegionsLen += foo.exonStarts[i+1] - foo.exonEnds[i] 
+                
+    else:
+        print "Thar be dragons - snfGene createGene strand does not match + or -"
+	
+    return foo 
+
